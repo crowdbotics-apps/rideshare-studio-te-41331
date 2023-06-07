001fb2ac6df3813c62b43eb0d5a98c18e8d894ca
Crowdbotics <team@crowdbotics.com>
2023-06-07 15:17:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rideshare-studio-te-41331",
+  url: "https://rideshare-studio-te-41331.botics.co",
+  api: "https://rideshare-studio-te-41331.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
