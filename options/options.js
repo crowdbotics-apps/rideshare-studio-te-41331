c6f2e2cfa9443a3b47622f0ce683bab1d967ca90
iniobong Etuk <iniobong.etuk@crowdbotics.com>
2023-06-08 17:22:17 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ export const globalOptions = {
   "name": "rideshare-studio-te-41331",
   "url": "https://rideshare-studio-te-41331.botics.co",
   "api": "https://rideshare-studio-te-41331.botics.co/api/v1",
-  "initialRoute": "ScreenAI10"
+  "initialRoute": "ScreenAI2"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
