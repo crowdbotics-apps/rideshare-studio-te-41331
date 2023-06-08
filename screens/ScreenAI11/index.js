c982dd0acec060e4dfa45b8f00bb6bb8a743381a
iniobong Etuk <iniobong.etuk@crowdbotics.com>
2023-06-08 17:37:48 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI11/index.js
+++ screens/ScreenAI11/index.js
@@ -23,7 +23,9 @@ const Dashboard = () => {
             <Image style={styles.menuItemImage} source={{
             uri: 'https://tinyurl.com/42evm3m3'
           }} />
-            <Text style={styles.menuItemText}>Menu 2</Text>
+            <Pressable onPress={() => {
+            navigation.navigate("ScreenAI20");
+          }}><Text style={styles.menuItemText}>{"Messages"}</Text></Pressable>
           </TouchableOpacity>
           <TouchableOpacity style={styles.menuItem}>
             <Image style={styles.menuItemImage} source={{
