95dd6d76afd52190855e34ee1101e21965faaef1
Crowdbotics <engineering@crowdbotics.com>
2023-10-20 15:06:07 +0000

    Update store.

--- store/ridesharestudioteAPI/api.js
+++ store/ridesharestudioteAPI/api.js
@@ -9,13 +9,13 @@ function api_docs_schema_retrieve(payload) {
   })
 }
 function api_v1_login_create(payload) {
-  return ridesharestudioteAPI.post(`/api/v1/login/`, payload.data)
+  return ridesharestudioteAPI.post(`/api/v1/login/`, payload)
 }
 function api_v1_signup_create(payload) {
-  return ridesharestudioteAPI.post(`/api/v1/signup/`, payload.data)
+  return ridesharestudioteAPI.post(`/api/v1/signup/`, payload)
 }
 function rest_auth_login_create(payload) {
-  return ridesharestudioteAPI.post(`/rest-auth/login/`, payload.data)
+  return ridesharestudioteAPI.post(`/rest-auth/login/`, payload)
 }
 function rest_auth_logout_retrieve(payload) {
   return ridesharestudioteAPI.get(`/rest-auth/logout/`)
@@ -24,34 +24,34 @@ function rest_auth_logout_create(payload) {
   return ridesharestudioteAPI.post(`/rest-auth/logout/`)
 }
 function rest_auth_password_change_create(payload) {
-  return ridesharestudioteAPI.post(`/rest-auth/password/change/`, payload.data)
+  return ridesharestudioteAPI.post(`/rest-auth/password/change/`, payload)
 }
 function rest_auth_password_reset_create(payload) {
-  return ridesharestudioteAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return ridesharestudioteAPI.post(`/rest-auth/password/reset/`, payload)
 }
 function rest_auth_password_reset_confirm_create(payload) {
   return ridesharestudioteAPI.post(
     `/rest-auth/password/reset/confirm/`,
-    payload.data
+    payload
   )
 }
 function rest_auth_registration_create(payload) {
-  return ridesharestudioteAPI.post(`/rest-auth/registration/`, payload.data)
+  return ridesharestudioteAPI.post(`/rest-auth/registration/`, payload)
 }
 function rest_auth_registration_verify_email_create(payload) {
   return ridesharestudioteAPI.post(
     `/rest-auth/registration/verify-email/`,
-    payload.data
+    payload
   )
 }
 function rest_auth_user_retrieve(payload) {
   return ridesharestudioteAPI.get(`/rest-auth/user/`)
 }
 function rest_auth_user_update(payload) {
-  return ridesharestudioteAPI.put(`/rest-auth/user/`, payload.data)
+  return ridesharestudioteAPI.put(`/rest-auth/user/`, payload)
 }
 function rest_auth_user_partial_update(payload) {
-  return ridesharestudioteAPI.patch(`/rest-auth/user/`, payload.data)
+  return ridesharestudioteAPI.patch(`/rest-auth/user/`, payload)
 }
 export const apiService = {
   api_docs_schema_retrieve,
